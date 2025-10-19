bb7c7e713141993b61228b8112acdf970130281b
Fortune Domination <fortunedomination@gmail.com>
2025-10-19 23:19:55 +0000

    It's not displaying anything thing

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,19 +1,27 @@
+'use client';
+
 import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState<boolean>(false)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+    // This function will only run on the client
+    const checkDevice = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    };
+
+    // Initial check
+    checkDevice();
+
+    // Listen for window resize
+    window.addEventListener("resize", checkDevice);
+
+    // Cleanup listener on component unmount
+    return () => window.removeEventListener("resize", checkDevice);
   }, [])
 
-  return !!isMobile
+  return isMobile;
 }
