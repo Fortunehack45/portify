c2ebd6f96b91e71214b47d7b0c4cf4a1413b88b5
Fortune Domination <fortunedomination@gmail.com>
2025-10-20 19:06:20 +0000

    20:04:00.569 Running build in Portland, USA (West) – pdx1 20:04:00.570 B

--- src/app/[username]/portfolio/[portfolioSlug]/page.tsx
+++ src/app/[username]/portfolio/[portfolioSlug]/page.tsx
@@ -18,6 +18,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { useToast } from '@/hooks/use-toast';
 import { errorEmitter } from '@/firebase/error-emitter';
